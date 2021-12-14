76fb3d13e2b803a869fbc3dc0cdbc6805a90ac4d
Asmenys <76791232+Asmenys@users.noreply.github.com>
2021-12-14 21:22:36 +0000

    Diagonal movements for bishop

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -40,7 +40,7 @@ end
 
 class Rook < Piece
   def possible_moves(location)
-    possible_moves = [moves_on_axis(location, 0), moves_on_axis(location, 1)]
+    move_array = [moves_on_axis(location, 0), moves_on_axis(location, 1)]
   end
 
   def moves_on_axis(location, movement_index)
@@ -61,6 +61,45 @@ class Rook < Piece
 end
 
 class Bishop < Piece
+  def possible_moves(_location)
+    move_array = []
+  end
+
+  def diagonal_movement(location); end
+
+  def modify_dimension(dimension, index)
+    array = []
+    array << dimension += index while dimension >= 1 && dimension <= 6
+    array
+  end
+
+  def diagonal_movement(location, index_one, _index_two)
+    movement_array = []
+    movement_array << filter_the_array(generate_sub_array(location, index_one, index_one)).reverse
+    movement_array << filter_the_array(generate_sub_array(location, index_one * -1, index_one * -1))
+    movement_array
+  end
+
+  def generate_sub_array(location, index_one, index_two)
+    row_modifications = modify_dimension(location[0], index_one)
+    column_modifications = modify_dimension(location[1], index_two)
+    result_array = conjoin_the_arrays(row_modifications, column_modifications)
+  end
+
+  def filter_the_array(array)
+    array.each do |sub_array|
+      array.delete(sub_array) if sub_array.include?(nil)
+    end
+    array
+  end
+
+  def conjoin_the_arrays(row_array, column_array)
+    conjoined_array = []
+    row_array.each_with_index do |row, index|
+      conjoined_array << [row, column_array[index]]
+    end
+    conjoined_array
+  end
 end
 
 class Knight < Piece
--- spec/piece_tests/piece_spec.rb
+++ spec/piece_tests/piece_spec.rb
@@ -1,21 +1,48 @@
-require 'pieces.rb'
+# frozen_string_literal: true
 
-    describe Pawn do
-        describe '#two_step' do
-        subject(:pawn) {described_class.new("black", "pawn")}
-            context 'if the piece has not yet been moved, it can two step' do
-                it 'returns the would-be location when given its current location' do
-                    current_location = [4,3]
-                    expected_location = [6,3]
-                    expect(pawn.two_step(current_location)).to eq(expected_location)
-                end
-                it 'if the piece has been moved, should return nil' do
-                    current_location = [4,3]
-                    pawn.moved
-                    expect(pawn.two_step(current_location)).to be nil
-                end
-            end
-        end
+require 'pieces'
+
+describe Pawn do
+  describe '#two_step' do
+    subject(:pawn) { described_class.new('black', 'pawn') }
+    context 'if the piece has not yet been moved, it can two step' do
+      it 'returns the would-be location when given its current location' do
+        current_location = [4, 3]
+        expected_location = [6, 3]
+        expect(pawn.two_step(current_location)).to eq(expected_location)
+      end
+      it 'if the piece has been moved, should return nil' do
+        current_location = [4, 3]
+        pawn.moved
+        expect(pawn.two_step(current_location)).to be nil
+      end
     end
-
-
+  end
+end
+describe Bishop do
+  describe '#modify_dimension' do
+    subject(:bishop) { described_class.new('black', 'bishop') }
+    context 'it increments/decrements the given dimension while keeping it within bounds' do
+      it 'decrements the dimension' do
+        dimensino = 4
+        expected_array_result = [3, 2, 1, 0]
+        expect(bishop.modify_dimension(dimensino, -1)).to eq(expected_array_result)
+      end
+      it 'increments the dimension' do
+        dimensino = 4
+        expected_array_result = [5, 6, 7]
+        expect(bishop.modify_dimension(dimensino, 1)).to eq(expected_array_result)
+      end
+    end
+  end
+  describe '#diagonal_movement' do
+    subject(:bishop) { described_class.new('black', 'bishop') }
+    context 'it generates an array of movements along a diagonal left/right' do
+      it 'generates diagonal movements up to left' do
+        location = [4, 3]
+        expected_array_result = [[7, 6], [6, 5], [5, 4], [3, 2], [2, 1], [1, 0]]
+        expect(bishop.diagonal_movement(location, 1, 1)).to eq(expected_array_result)
+      end
+    end
+  end
+end
