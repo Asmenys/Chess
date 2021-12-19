7b7ad1b93020b4c6612acd13a4797cf048e7417e
Asmenys <76791232+Asmenys@users.noreply.github.com>
2021-12-19 01:25:22 +0000

    fixed display as it only displayed 7 rows, addded a Game class that currently only populates the given game board

--- lib/display.rb
+++ lib/display.rb
@@ -1,27 +1,25 @@
 # frozen_string_literal: true
-
-
 module Display
   def display_board
     puts color_board
   end
 
   def color_board
-    colored_board = ""
-    row_index = 1
-    last_colour = 5
-    while row_index <=7
+    colored_board = ''
+    row_index = 0
+    last_colour = get_background_color(last_colour)
+    while row_index <= 7
       colored_board += color_row(row_index, last_colour)
       last_colour = get_background_color(last_colour)
-      row_index+=1
+      row_index += 1
     end
-    colored_board += "    a    b    c    d    e    f    g    h  ".bold
+    colored_board += '  a  b  c  d  e  f  g  h '.bold
     colored_board
   end
 
   def color_row(row_index, last_colour)
     column_index = 1
-    coloured_row = "#{row_index}".bold
+    coloured_row = "#{row_index+1}"
     row = @board[row_index]
     row.each do |square|
       coloured_row += colorize_square(square, last_colour)
@@ -35,21 +33,21 @@ module Display
   def colorize_square(square, last_colour)
     background_color_index = get_background_color(last_colour)
     if square.nil?
-      coloured_square = '     '.bg_color(background_color_index)
+      coloured_square = '   '.bg_color(background_color_index)
     else
       text_colour_index = get_text_colour(square.team)
       color_piece = square.display_value.text_color(text_colour_index).bg_color(background_color_index)
-      colored_whitespace = " ".bg_color(background_color_index)
-      coloured_square = "#{colored_whitespace*2}#{color_piece}#{colored_whitespace*2}"
+      colored_whitespace = ' '.bg_color(background_color_index)
+      coloured_square = "#{colored_whitespace}#{color_piece}#{colored_whitespace}"
     end
     coloured_square
   end
 
   def get_background_color(last_colour)
-    if last_colour == 5
+    if last_colour == 2
       1
     else
-      5
+      2
     end
   end
 
---
+++ lib/game.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+require_relative 'board.rb'
+require_relative 'display.rb'
+require_relative 'moveset_modules.rb'
+require_relative 'piece_values.rb'
+require_relative 'pieces.rb'
+require_relative 'string.rb'
+class Game
+  def initialize(board = Board.new)
+    @board = board
+    populate_the_board
+  end
+  def populate_the_board
+    populate_the_knights
+    populate_the_pawns
+    populate_the_rooks
+    populate_the_bishops
+    populate_the_queens
+    populate_the_kings
+  end
+
+  def populate_the_knights
+    @board.add_piece('white', 'Knight', notation_to_location('g1'))
+    @board.add_piece('white', 'Knight', notation_to_location('b1'))
+    @board.add_piece('black', 'Knight', notation_to_location('b8'))
+    @board.add_piece('black', 'Knight', notation_to_location('g8'))
+  end
+
+  def populate_the_pawns
+    column_index = 97
+    while column_index <= 104
+      white_location = notation_to_location("#{column_index.chr}2")
+      black_location = notation_to_location("#{column_index.chr}7")
+      @board.add_piece('white', 'Pawn', white_location)
+      @board.add_piece('black', 'Pawn', black_location)
+      column_index += 1
+    end
+  end
+
+  def populate_the_rooks
+    @board.add_piece('white', 'Rook', notation_to_location('a1'))
+    @board.add_piece('white', 'Rook', notation_to_location('h1'))
+    @board.add_piece('black', 'Rook', notation_to_location('a8'))
+    @board.add_piece('black', 'Rook', notation_to_location('h8'))
+  end
+
+  def populate_the_bishops
+    @board.add_piece('white', 'Bishop', notation_to_location('c1'))
+    @board.add_piece('white', 'Bishop', notation_to_location('f1'))
+    @board.add_piece('black', 'Bishop', notation_to_location('c8'))
+    @board.add_piece('black', 'Bishop', notation_to_location('f8'))
+  end
+
+  def populate_the_queens
+    @board.add_piece('black', 'Queen', notation_to_location('e8'))
+    @board.add_piece('white', 'Queen', notation_to_location('e1'))
+end
+
+  def populate_the_kings
+    @board.add_piece('black', 'King', notation_to_location('d8'))
+    @board.add_piece('white', 'King', notation_to_location('d1'))
+  end
+  def show_board
+    @board.display_board
+  end
+
+  def notation_to_location(notation)
+    [notation[1].to_i-1, notation[0].ord - 97,]
+  end
+end
