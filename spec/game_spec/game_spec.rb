570365e9e322a234220968023228b490436a8606
Asmenys <76791232+Asmenys@users.noreply.github.com>
2021-12-29 17:39:37 +0000

    standardized return values from to use consistent types

--- spec/game_spec/game_spec.rb
+++ spec/game_spec/game_spec.rb
@@ -8,7 +8,8 @@ describe Game do
     context 'takes an array of board indexes and returns the values at given index on board' do
       it 'returns unmodified array when none of the indexes point to a value' do
         starting_array = [[4, 0], [4, 1], [4, 2], [4, 3], [4, 4], [4, 5], [4, 6], [4, 7]]
-        expected_result_array = starting_array
+        temp_index = -1
+        expected_result_array = Array.new(8) { {index: [4, temp_index+=1], piece_class: nil}}
         expect(game.return_square_values_from(starting_array)).to eq(expected_result_array)
       end
       it 'when all indexes point to a pawn returns an array full of pawns' do
