baa5f6a5925875cbc091cae0707cefb4cb9ecbbd
Asmenys <76791232+Asmenys@users.noreply.github.com>
2022-02-06 22:18:19 +0000

    #self_check? fixes

--- lib/self_check.rb
+++ lib/self_check.rb
@@ -10,8 +10,16 @@ class Self_check
     alternate_board = @board.clone
     piece = alternate_board.get_value_of_square(current_location)
     @alternate_game.board = alternate_board
-    kings_location = @alternate_game.find_king(piece.team[0])
-    @alternate_game.move_piece(current_location, movement_destination)
-    @alternate_game.is_king_in_check?(kings_location)
+    active_color = @alternate_game.fen_to_color
+    kings_location = @alternate_game.board.find_king(active_color)
+    move_piece(current_location, movement_destination)
+    @alternate_game.is_king_in_check?
+  end
+
+  def move_piece(current_location, movement_destination)
+    board = @alternate_game.board
+    piece = board.get_value_of_square(current_location)
+    board.set_square_to(current_location, nil)
+    board.set_square_to(movement_destination, piece)
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,17 +6,15 @@ require 'load_game'
 describe Game do
   describe '#would_leave_king_in_check?' do
     context 'returns a bool based on whether the move leaves own players king in check' do
-      xit 'when a pawn moves leaving the king in check for a bishop return true' do
+      it 'when a pawn moves leaving the king in check for a bishop return true' do
         game = Load_game.new('8/8/8/4b3/3P4/2K5/8/8 w - - 0 1').game
         current_loc = [4, 3]
-        kings_loc = game.find_king('b')
         destination_loc = [3, 3]
         expect(game.would_leave_king_in_check?(current_loc, destination_loc)).to be true
       end
-      xit 'when a pawn moves but the king is not left in check for a bishop' do
+      it 'when a pawn moves but the king is not left in check for a bishop' do
         game = Load_game.new('8/8/8/4b3/8/2KP4/8/8 w - - 0 1').game
         current_loc = [5, 3]
-        kings_loc = game.find_king('b')
         destination_loc = [4, 3]
         expect(game.would_leave_king_in_check?(current_loc, destination_loc)).to be false
       end
